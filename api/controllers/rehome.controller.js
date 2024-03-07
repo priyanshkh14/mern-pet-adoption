cedd54f51c7c3a4eb61326f7b11f8669fec7ce24
priyanshkh14 <priyanshkh14@gmail.com>
2024-03-07 17:55:05 +0530

    create update rehome api route

--- api/controllers/rehome.controller.js
+++ api/controllers/rehome.controller.js
@@ -1,4 +1,5 @@
 import Rehome from '../models/rehome.model.js';
+import { errorHandler } from '../utils/error.js';
 
 export const createRehome = async (req, res, next) => {
   try {
@@ -23,6 +24,27 @@ export const deleteRehome = async (req, res, next) => {
   try {
     await Rehome.findByIdAndDelete(req.params.id);
     res.status(200).json('Rehome has been deleted!');
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateRehome = async (req, res, next) => {
+  const rehome = await Rehome.findById(req.params.id);
+  if (!rehome) {
+    return next(errorHandler(404, 'Rehome not found!'));
+  }
+  if (req.user.id !== rehome.userRef) {
+    return next(errorHandler(401, 'You can only update your own rehome animal!'));
+  }
+
+  try {
+    const updatedRehome = await Rehome.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json(updatedRehome);
   } catch (error) {
     next(error);
   }
--- api/routes/rehome.route.js
+++ api/routes/rehome.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { createRehome, deleteRehome } from '../controllers/rehome.controller.js';
+import { createRehome, deleteRehome, updateRehome } from '../controllers/rehome.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 router.post('/create', verifyToken, createRehome);
 router.delete('/delete/:id', verifyToken, deleteRehome);
+router.post('/update/:id', verifyToken, updateRehome);
 
 export default router;
