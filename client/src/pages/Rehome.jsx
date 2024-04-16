dc4f0c29d0968cae94b1d5496f1e55b2bcba355f
priyanshkh14 <priyanshkh14@gmail.com>
2024-04-16 21:43:54 +0530

    complete rehome page

--- client/src/pages/Rehome.jsx
+++ client/src/pages/Rehome.jsx
@@ -4,12 +4,22 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore from 'swiper';
 import { Navigation } from 'swiper/modules';
 import 'swiper/css/bundle';
+import {
+  FaDog,
+  FaGenderless,
+  FaHome,
+  FaPalette,
+  FaShare,
+  FaSyringe,
+  FaTransgender,
+} from 'react-icons/fa';
 
 export default function Rehome() {
   SwiperCore.use([Navigation]);
   const [rehome, setRehome] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
+  const [copied, setCopied] = useState(false);
   const params = useParams();
   useEffect(() => {
     const fetchRehome = async () => {
@@ -55,6 +65,71 @@ export default function Rehome() {
               </SwiperSlide>
             ))}
           </Swiper>
+
+
+          <div className='fixed top-[13%] right-[3%] z-10 border rounded-full w-12 h-12 flex justify-center items-center bg-slate-100 cursor-pointer'>
+          <FaShare
+              className='text-slate-500'
+              onClick={() => {
+                navigator.clipboard.writeText(window.location.href);
+                setCopied(true);
+                setTimeout(() => {
+                  setCopied(false);
+                }, 2000);
+              }}
+            />
+          </div>    
+          {copied && (
+            <p className='fixed top-[23%] right-[5%] z-10 rounded-md bg-slate-100 p-2'>
+              Link copied!
+            </p>
+          )}
+          <div className='flex flex-col max-w-4xl mx-auto p-3 my-7 gap-4'>
+          <p className='text-2xl font-semibold'>
+            {rehome.name}
+          </p>
+          <p className='flex items-center mt-6 gap-2 text-red-600  text-lg'>
+          <FaHome className='text-green-700' />
+            Rehome
+          </p>
+          <div className='flex gap-4'>
+              <p className='bg-red-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
+                Breed : {rehome.breed}
+              </p>
+              <p className='bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
+                  Age : {rehome.age} years
+                </p>
+          </div> 
+          <p className='text-slate-800'>
+            <span className='font-semibold text-black'>Description - </span>
+            {rehome.description}
+          </p>  
+           <ul className=' text-green-900 font semibold text-sm flex flex-wrap items-center gap-4 sm:gap-6'>
+            <li className='flex items-center gap-1 whitespace-nowrap'>
+              <FaDog className='text-lg'/>
+              {rehome.species}
+            </li>
+            <li className='flex items-center gap-1 whitespace-nowrap'>
+              <FaSyringe className='text-lg'/>
+              {rehome.vaccinated ? 'Vaccinated' : 'Not Vaccinated'}
+            </li>
+            <li className='flex items-center gap-1 whitespace-nowrap'>
+              <FaPalette className='text-lg'/>
+              {rehome.color}
+            </li>
+            <li className='flex items-center gap-1 whitespace-nowrap'>
+              <FaGenderless className='text-lg'/>
+              {rehome.gender}
+            </li>
+            <li className='flex items-center gap-1 whitespace-nowrap'>
+              <FaTransgender className='text-lg'/>
+              {rehome.gender}
+            </li>
+           </ul>
+          </div>
+
+
+
         </div>
       )}
     </main>
